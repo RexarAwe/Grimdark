c215b53fe4f9b202515f8bed7268b0ea43def353
Gavin Kijkul <gavinkijkul@gmail.com>
2021-03-03 14:52:16 +0700

    Based on Sot5, started adjusting game flow

---
+++ GameManager.cs
@@ -0,0 +1,84 @@
+﻿// managers the game flow and all other components
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject penguinViking;
+
+    public int turn;
+    [SerializeField] public List<GameObject> units;
+    private int unitID = 0;
+
+    private MapManager mapManager;
+    private GameObject StartCombatBT;
+    private GameObject EndCombatBT;
+
+    void Start()
+    {
+        mapManager = GameObject.Find("Map Manager").GetComponent<MapManager>();
+        StartCombatBT = GameObject.Find("Start Combat BT");
+        EndCombatBT = GameObject.Find("End Combat BT");
+        StartCombatBT.SetActive(false);
+        EndCombatBT.SetActive(false);
+
+        mapManager.Init();
+        SpawnPC();
+
+        units[0].GetComponent<UnitMovement>().Move();
+    }
+
+    public void InitBattle()
+    {
+        // roll to see initiative
+    }
+
+    public void EndBattle()
+    {
+        units[0].GetComponent<UnitGeneral>().SetOOC(true);
+        StartCombatBT.SetActive(false);
+        EndCombatBT.SetActive(false);
+
+        units[0].GetComponent<UnitMovement>().Move();
+    }
+
+    private void SpawnPC()
+    {
+        GameObject unit;
+        UnitGeneral generalUnit;
+        UnitMovement movementUnit;
+        UnitCombat combatUnit;
+
+        unit = Instantiate(penguinViking, new Vector3(0, 0, 0), transform.rotation);
+
+        movementUnit = unit.GetComponent<UnitMovement>();
+        movementUnit.Init(2);
+
+        Debug.Log("Unit location: " + movementUnit.GetLoc());
+
+        generalUnit = unit.GetComponent<UnitGeneral>();
+        generalUnit.Init(unitID, 0, 10, 2);
+
+        combatUnit = unit.GetComponent<UnitCombat>();
+        combatUnit.Init();
+
+        AddUnit(unit);
+        unitID++;
+    }
+
+    public void AddUnit(GameObject unit) // add the unit to the unit tracking list
+    {
+        units.Add(unit);
+    }
+
+    public void CombatTrigger()
+    {
+        units[0].GetComponent<UnitGeneral>().SetOOC(false);
+        StartCombatBT.SetActive(true);
+        EndCombatBT.SetActive(true);
+    }
+}
---
+++ UnitGeneral.cs
@@ -0,0 +1,89 @@
+﻿// Unit status effects, hp, and exp
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitGeneral : MonoBehaviour
+{
+    [SerializeField] private int id;
+    [SerializeField] private int allegiance; // friend or enemy
+    [SerializeField] private int hp;
+    [SerializeField] private int actions;
+    [SerializeField] private int remActions;
+    [SerializeField] private int status;
+
+    [SerializeField] private bool OOC; // out of combat
+
+    UnitMovement movementUnit;
+
+    //[SerializeField] private GameObject hpBar; // prefab storage
+    //private GameObject healthBar; // actual reference
+    //private HpBar hpBarScript;
+
+    public void Init(int id_val, int allegiance_val, int hp_val, int actions_val)
+    {
+        id = id_val;
+        allegiance = allegiance_val;
+        hp = hp_val;
+        actions = actions_val;
+        remActions = actions_val;
+        OOC = true;
+    }
+
+    // focus camera on this unit
+    public void Focus()
+    {
+        Camera.main.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, Camera.main.transform.position.z);
+    }
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+    public int GetID()
+    {
+        return id;
+    }
+
+    public int GetStatus()
+    {
+        return status;
+    }
+
+    public int GetActions()
+    {
+        return actions;
+    }
+
+    public void SetActions(int actions_val)
+    {
+        actions = actions_val;
+    }
+
+    public int GetRemActions()
+    {
+        return remActions;
+    }
+
+    public void SetRemActions(int act_val)
+    {
+        remActions = act_val;
+    }
+
+    public void RestoreActions()
+    {
+        remActions = actions;
+    }
+
+    public bool GetOOC()
+    {
+        return OOC;
+    }
+
+    public void SetOOC(bool val)
+    {
+        OOC = val;
+    }
+}
